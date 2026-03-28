8384e28cac58eca1c7754636877aa40e87bd6834
shadow-dom <jacob.r.holcomb+1@gmail.com>
2026-03-28 10:30:26 -0400

    Added example with query + form submission.

--- go/chat-api/main.go
+++ go/chat-api/main.go
@@ -58,5 +58,15 @@ func main() {
 		})
 	})
 
+	router.POST("/post", func(c *gin.Context) {
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		name := c.PostForm("name")
+		message := c.PostForm("message")
+
+		fmt.Printf("id: %s; page: %s; name: %s; message: %s\n", id, page, name, message)
+		c.String(http.StatusOK, "id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+	})
+
 	router.Run(":8080")
 }
