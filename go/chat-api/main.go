d98bed9ad1d7af3b17e41d4382603bcbf417a017
shadow-dom <jacob.r.holcomb+1@gmail.com>
2026-03-27 21:48:04 -0400

    Testing Gin's routing.

--- go/chat-api/main.go
+++ go/chat-api/main.go
@@ -1,13 +1,54 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getting(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"method": "GET"})
+}
+
+func posting(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"method": "POST"})
+}
+
+func putting(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"method": "PUT"})
+}
+
+func deleting(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"method": "DELETE"})
+}
+
+func patching(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"method": "PATCH"})
+}
+
+func head(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+func options(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
 
 func main() {
+	// Creates a gin router with default middleware:
+	// logger and recovery (crash-free) middleware
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // listens on 0.0.0.0:8080 by default
+
+	router.GET("/someGet", getting)
+	router.POST("/somePost", posting)
+	router.PUT("/somePut", putting)
+	router.DELETE("/someDelete", deleting)
+	router.PATCH("/somePatch", patching)
+	router.HEAD("/someHead", head)
+	router.OPTIONS("/someOptions", options)
+
+	// By default it serves on :8080 unless a
+	// PORT environment variable was defined.
+	router.Run()
+	// router.Run(":3000") for a hard coded port
 }
